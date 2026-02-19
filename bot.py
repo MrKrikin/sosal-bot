42619c0c23b16034763a498de49384a70211873f
MrKrikin <artemzakharov13@gmail.com>
2026-02-19 09:45:43 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+﻿import os
+import telebot
+
+TOKEN = os.environ.get('TOKEN')
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.chat.type in ['group', 'supergroup']:
+        message_text = message.text.lower() if message.text else ''
+        
+        if 'сосал' in message_text:
+            bot.reply_to(message, "да")
+        elif message_text in ['да', 'lf', 'da']:
+            bot.reply_to(message, "Сосал?")
+
+print("Бот запущен...")
+bot.infinity_polling()
